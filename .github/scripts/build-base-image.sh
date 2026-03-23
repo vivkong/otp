6780c4c62968054a0529491e944a039535891566
Vivian Kong <vivkong@ca.ibm.com>
2026-03-23 00:51:33 +0000

    remove docker image name change

--- .github/scripts/build-base-image.sh
+++ .github/scripts/build-base-image.sh
@@ -63,7 +63,7 @@ case "${BASE_TAG}" in
         BASE_TYPE=debian-base
         ;;
     *ubuntu-base)
-        BASE="docker-na.artifactory.swg-devops.com/wcp-lozoseco-team-docker-remote/ubuntu:22.04"
+        BASE="ubuntu:22.04"
         BASE_TYPE=ubuntu-base
         ;;
 esac
